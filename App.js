95422794d021f6c56dfa9801d9414ad4b3ade4d0
SamrithaS <62285891+SamrithaS@users.noreply.github.com>
2020-03-18 17:08:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Stack, ThemeProvider, MenuGroup, Flex } from "@chakra-ui/core";
+import "./App.css";
+import { FiImage } from "react-icons/fi";
+import { TiLocationOutline } from "react-icons/ti";
+import { IoIosPricetag } from "react-icons/io";
+import { GoSettings } from "react-icons/go";
+import { FaBuilding } from "react-icons/fa";
+
+import EachMenuItem from "./components/header.js";
+function App() {
+  return (
+    <ThemeProvider>
+      <Stack
+        mt={10}
+        maxW={1200}
+        mx="auto"
+        direction="row"
+        justifyContent="space-between"
+      >
+        <MenuGroup>
+          <Flex>
+            <EachMenuItem
+              leftIcon={FiImage}
+              menuItemName="Type"
+              TagNumber={374}
+            />
+             <EachMenuItem
+              leftIcon={TiLocationOutline}
+              menuItemName="Location"
+              TagNumber={20}
+            />
+             <EachMenuItem
+              leftIcon={FaBuilding}
+              menuItemName="Apartment"
+              TagNumber={374}
+            />
+             <EachMenuItem
+              leftIcon={IoIosPricetag}
+              menuItemName="Tag"
+              TagNumber={178}
+            />
+             <EachMenuItem
+              leftIcon={FiImage}
+              menuItemName="MoreFilters"
+            />
+            
+          </Flex>
+        </MenuGroup>
+        <EachMenuItem  menuItemName="sort by Recommended" />
+      </Stack>
+    </ThemeProvider>
+  );
+}
+
+export default App;
