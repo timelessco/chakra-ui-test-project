f9cac0048749c873fae1a5c350825e3256f15f62
SamrithaS <62285891+SamrithaS@users.noreply.github.com>
2020-03-18 17:29:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-import React from "react";
-import {
-  Menu,
-  MenuButton,
-  MenuGroup,
-  Stack,
-  Icon,
-  ThemeProvider,
-  Box,
-  Tag,
-  Flex
-} from "@chakra-ui/core";
+import React, { Component } from "react";
+import { Stack, ThemeProvider, MenuGroup, Flex } from "@chakra-ui/core";
 import "./App.css";
 import { FiImage } from "react-icons/fi";
 import { TiLocationOutline } from "react-icons/ti";
@@ -17,132 +7,50 @@ import { IoIosPricetag } from "react-icons/io";
 import { GoSettings } from "react-icons/go";
 import { FaBuilding } from "react-icons/fa";
 
+import EachMenuItem from "./components/header.js";
 function App() {
   return (
-    <Stack mt={40} maxW={1200} mx="auto">
-      <Header />;
-    </Stack>
-  );
-}
-function Header() {
-  return (
     <ThemeProvider>
-      <Stack direction="row" justifyContent="space-between">
+      <Stack
+        mt={10}
+        maxW={1200}
+        mx="auto"
+        direction="row"
+        justifyContent="space-between"
+      >
         <MenuGroup>
           <Flex>
-            <Menu>
-              <Flex alignItems="center">
-                <Box as={FiImage} pb={1}/>
-                <MenuButton border="none" pr={5} fontSize={14}>
-                  Type
-                  <Tag
-                    fontSize={9}
-                    color="gray.500"
-                    size="sm"
-                    pr={0}
-                    pb={0}
-                    pt={0}
-                    ml={1}
-                    rounded="full"
-                    variant="subtle"
-                    variantColor="gray"
-                  >
-                    374
-                  </Tag>
-                  <Icon name="chevron-down" />
-                </MenuButton>
-              </Flex>
-            </Menu>
-            <Menu>
-              <Flex alignItems="center">
-                <Box as={TiLocationOutline} pb={1}/>
-                <MenuButton border="none" pr={5} fontSize={14}>
-                  Location
-                  <Tag
-                    fontSize={9}
-                    color="gray.500"
-                    size="sm"
-                    pr={0}
-                    pl={3}
-                    pb={0}
-                    pt={0}
-                    ml={1}
-                    rounded="full"
-                    variant="subtle"
-                    variantColor="gray"
-                  >
-                    20
-                  </Tag>
-                  <Icon name="chevron-down" />
-                </MenuButton>
-              </Flex>
-            </Menu>
-            <Menu>
-              <Flex alignItems="center">
-                <Box as={FaBuilding} pr={1} pb={1}/>
-                <MenuButton border="none" pr={5} pl={0} fontSize={14}>
-                  Apartment
-                  <Tag
-                    fontSize={9}
-                    color="gray.500"
-                    size="sm"
-                    pr={0}
-                    pb={0}
-                    pt={0}
-                    ml={1}
-                    rounded="full"
-                    variant="subtle"
-                    variantColor="gray"
-                  >
-                    374
-                  </Tag>
-                  <Icon name="chevron-down" />
-                </MenuButton>
-              </Flex>
-            </Menu>
-            <Menu>
-              <Flex alignItems="center">
-                <Box as={IoIosPricetag}  pb={1}   />
-            <MenuButton border="none" pr={5} pl={1} fontSize={14}>
-                  Tag
-                  <Tag
-                    fontSize={9}
-                    color="gray.500"
-                    size="sm"
-                    pr={0}
-                    pl={3}
-                    pb={0}
-                    pt={0}
-                    ml={1}
-                    rounded="full"
-                    variant="subtle"
-                    variantColor="gray"
-                  >
-                    178
-                  </Tag>
-                  <Icon name="chevron-down" />
-                </MenuButton>
-              </Flex>
-            </Menu>
-            <Menu>
-              <Flex alignItems="center">
-                <Box as={GoSettings} pr={1} pb={1}/>
-                <MenuButton border="none" fontSize={14}>
-                  MoreFilters
-                  <Icon pl={2} name="chevron-down" />
-                </MenuButton>
-              </Flex>
-            </Menu>
+            <EachMenuItem
+              leftIcon={FiImage}
+              menuItemName="Type"
+              TagNumber={374}
+            />
+             <EachMenuItem
+              leftIcon={TiLocationOutline}
+              menuItemName="Location"
+              TagNumber={20}
+            />
+             <EachMenuItem
+              leftIcon={FaBuilding}
+              menuItemName="Apartment"
+              TagNumber={374}
+            />
+             <EachMenuItem
+              leftIcon={IoIosPricetag}
+              menuItemName="Tag"
+              TagNumber={178}
+            />
+             <EachMenuItem
+              leftIcon={FiImage}
+              menuItemName="MoreFilters"
+            />
+            
           </Flex>
         </MenuGroup>
-        <Menu>
-          <MenuButton border="none" direction="row" fontSize={14}>
-            sort by Recomended
-            <Icon name="chevron-down" />
-          </MenuButton>
-        </Menu>
+        <EachMenuItem  menuItemName="sort by Recommended" />
       </Stack>
     </ThemeProvider>
   );
 }
+
 export default App;
